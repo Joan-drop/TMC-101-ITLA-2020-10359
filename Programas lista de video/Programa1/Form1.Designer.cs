f1effc39c3a33af2698b0d32680a740474fa41bf
Joan Fermin Valdez Alcantara <123091113+Joan-drop@users.noreply.github.com>
2023-04-22 15:05:31 -0400

    Add files via upload

--- Programas lista de video/Programa1/Form1.Designer.cs
+++ Programas lista de video/Programa1/Form1.Designer.cs
@@ -1,16 +1,16 @@
-﻿namespace Programa01_01
+﻿namespace Programa_1._1
 {
     partial class Form1
     {
         /// <summary>
-        ///  Required designer variable.
+        /// Variable del diseñador necesaria.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        ///  Clean up any resources being used.
+        /// Limpiar los recursos que se estén usando.
         /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,67 +20,70 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
+        #region Código generado por el Diseñador de Windows Forms
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
         /// </summary>
         private void InitializeComponent()
         {
-            btnSaludo = new Button();
-            lblMensaje = new Label();
-            btnDespedida = new Button();
-            SuspendLayout();
+            this.btnSaludo = new System.Windows.Forms.Button();
+            this.LblMensaje = new System.Windows.Forms.Label();
+            this.btnDespedida = new System.Windows.Forms.Button();
+            this.SuspendLayout();
             // 
             // btnSaludo
             // 
-            btnSaludo.Location = new Point(254, 61);
-            btnSaludo.Name = "btnSaludo";
-            btnSaludo.Size = new Size(111, 41);
-            btnSaludo.TabIndex = 0;
-            btnSaludo.Text = "Saludo!";
-            btnSaludo.UseVisualStyleBackColor = true;
-            btnSaludo.Click += btnSaludo_Click;
+            this.btnSaludo.Location = new System.Drawing.Point(262, 68);
+            this.btnSaludo.Name = "btnSaludo";
+            this.btnSaludo.Size = new System.Drawing.Size(75, 23);
+            this.btnSaludo.TabIndex = 0;
+            this.btnSaludo.Text = "Saludo";
+            this.btnSaludo.UseVisualStyleBackColor = true;
+            this.btnSaludo.Click += new System.EventHandler(this.btnSaludo_Click);
             // 
-            // lblMensaje
+            // LblMensaje
             // 
-            lblMensaje.AutoSize = true;
-            lblMensaje.Location = new Point(45, 71);
-            lblMensaje.Name = "lblMensaje";
-            lblMensaje.Size = new Size(50, 20);
-            lblMensaje.TabIndex = 1;
-            lblMensaje.Text = "label1";
+            this.LblMensaje.AutoSize = true;
+            this.LblMensaje.Location = new System.Drawing.Point(33, 73);
+            this.LblMensaje.Name = "LblMensaje";
+            this.LblMensaje.Size = new System.Drawing.Size(35, 13);
+            this.LblMensaje.TabIndex = 1;
+            this.LblMensaje.Text = "label1";
             // 
             // btnDespedida
             // 
-            btnDespedida.Location = new Point(254, 172);
-            btnDespedida.Name = "btnDespedida";
-            btnDespedida.Size = new Size(111, 42);
-            btnDespedida.TabIndex = 2;
-            btnDespedida.Text = "Despedida";
-            btnDespedida.UseVisualStyleBackColor = true;
-            btnDespedida.Click += btnDespedida_Click;
+            this.btnDespedida.Location = new System.Drawing.Point(262, 110);
+            this.btnDespedida.Name = "btnDespedida";
+            this.btnDespedida.Size = new System.Drawing.Size(75, 23);
+            this.btnDespedida.TabIndex = 2;
+            this.btnDespedida.Text = "Despedida";
+            this.btnDespedida.UseVisualStyleBackColor = true;
+            this.btnDespedida.Click += new System.EventHandler(this.btnDespedida_Click);
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(413, 389);
-            Controls.Add(btnDespedida);
-            Controls.Add(lblMensaje);
-            Controls.Add(btnSaludo);
-            Name = "Form1";
-            Text = "Programa01";
-            Load += Form1_Load;
-            ResumeLayout(false);
-            PerformLayout();
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(390, 189);
+            this.Controls.Add(this.btnDespedida);
+            this.Controls.Add(this.LblMensaje);
+            this.Controls.Add(this.btnSaludo);
+            this.Name = "Form1";
+            this.Text = "Programa 1.1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
 
-        private Button btnSaludo;
-        private Label lblMensaje;
-        private Button btnDespedida;
+        private System.Windows.Forms.Button btnSaludo;
+        private System.Windows.Forms.Label LblMensaje;
+        private System.Windows.Forms.Button btnDespedida;
     }
 }
+
